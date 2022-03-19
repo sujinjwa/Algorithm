f76350ffdc834f8ac477a8a235dc85af8a098abb
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-03-19 23:52:59 +0900

    Create recursion2.py
    재귀함수 기본 문제 2

---
+++ recursion2.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+def increase(n):
+    if n == 0:
+        return
+
+    increase(n-1)
+    print(n, end=' ')
+
+def decrease(n):
+    if n == 0:
+        return
+    
+    print(n, end=' ')
+    decrease(n-1)
+
+increase(n)
+print()
+decrease(n)
