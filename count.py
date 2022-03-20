b053dcce381ec115325ba7a2b0a7e59d2001ebdc
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-03-20 23:40:48 +0900

    Create count.py
    재귀 함수 실행 횟수 count

---
+++ count.py
@@ -0,0 +1,17 @@
+n = int(input())
+cnt = 0
+
+def f(n):
+    global cnt
+    if n == 1:
+        return 1
+
+    if n % 2 == 0:
+        cnt += 1
+        return f(n//2)
+    else:
+        cnt += 1
+        return f(n//3)
+
+f(n)
+print(cnt)
