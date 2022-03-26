68b916e29e582fe6ebf06bfd71ea13d6c81f5225
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-03-26 22:05:07 +0900

    Create lambda_sort.py
    lambda 함수 이용하여 객체 정렬

---
+++ lambda_sort.py
@@ -0,0 +1,17 @@
+n = int(input())
+
+class Person:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+people = []
+for _ in range(n):
+    name, height, weight = tuple(input().split())
+    people.append(Person(name, int(height), int(weight)))
+
+people.sort(key=lambda x : x.height)
+
+for person in people:
+    print(person.name, person.height, person.weight)
