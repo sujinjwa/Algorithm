a5a2c2f93c24e84683141defe677928202aec2f2
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-04-13 00:02:30 +0900

    Update findMaxSection.py
    최대로 겹치는 지점 구하는 문제. 수정함.

--- findMaxSection.py
+++ findMaxSection.py
@@ -1,17 +1,16 @@
 n = int(input())
-arr = [0] * 100
+arr = [0] * 101
 
 for _ in range(n):
     start, end = tuple(map(int, input().split()))
     for i in range(start, end+1):
         arr[i] += 1
 
-print(arr)
-
-idx = 0
+max_idx = 1
 max_cnt = 0
-for i in range(1, len(arr)):
-    if arr[i] > arr[idx]:
-        max_cnt = arr[i]
+for j in range(2, len(arr)):
+    if arr[j] > arr[max_idx]:
+        max_idx = j
+    max_cnt = arr[max_idx]
 
 print(max_cnt)
