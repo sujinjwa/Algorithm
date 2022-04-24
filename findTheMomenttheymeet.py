61f43cea65e96f72fc18069fb0abbd9b97e72311
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-04-24 17:45:32 +0900

    Update findTheMomenttheymeet.py

--- findTheMomenttheymeet.py
+++ findTheMomenttheymeet.py
@@ -1,30 +1,30 @@
-MAX_T = 1000000
+MAX_T = 1000000 # 주어진 T의 단위가 1 <= t <= 1,000 이므로
 
-# 변수 선언 및 입력
 n, m = tuple(map(int, input().split()))
-pos_a, pos_b = [0] * (MAX_T + 1), [0] * (MAX_T + 1)
+pos_a, pos_b = [0] * (MAX_T + 1) , [0] * (MAX_T + 1)
 
-# A가 매 초마다 서있는 위치를 기록
-time_a = 1 # 흐른 시간 나타내는 변수
+time_a = 1 # 흐른 시간을 나타내는 변수
+# A가 매 초마다 서있는 위치 기록하기 위한 반복문
 for _ in range(n):
-		d, t = tuple(input().split()) # 이동하는 방향 나타내는 변수 d와 이동량 나타내는 변수 t
-		for _ in range(int(t)):
-				pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
-				time_a += 1
+    d, t = input().split()
+    for _ in range(int(t)):
+        # 1초 전 위치(pos_a[time_a - 1]에서 입력받은 d가 R이면 +1을, L이면 -1을 더해줌
+        pos_a[time_a] = pos_a[time_a - 1] + (1 if d == 'R' else -1)
+        time_a += 1 # 1초 흘렀음을 나타내기 위해 +1
 
-# B가 매 초마다 서있는 위치를 기록
 time_b = 1
+# B가 매 초마다 서있는 위치 기록하기 위한 반복문
 for _ in range(m):
-		d, t = tuple(input().split())
-		for _ in range(int(t)):
-				pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
-				time_b += 1
+    d, t = input().split()
+    for _ in range(int(t)):
+        pos_b[time_b] = pos_b[time_b - 1] + (1 if d == 'R' else -1)
+        time_b += 1
 
-# 최초로 만나는 시간을 구합니다.
+# 최초로 만나는 시간 구하기
 ans = -1
-for i in range(1, time_a):
-		if pos_a[i] == pos_b[i]:
-				ans = i
-				break
+for i in range(1, time_b):
+    if pos_a[i] == pos_b[i]:
+        ans = i
+        break
 
 print(ans)
