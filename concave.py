01aeff1594c77d06a4c8321a1e49a9ffca03e856
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-05-15 03:40:25 +0900

    Create concave.py
    19 * 19 크기의 바둑판에서 같은 색의 바둑 알이 가로, 세로, 그리고 대각선 방향으로 연속적으로 5개 놓이면 그 색이 이긴다고 할 때, 첫 번째 줄에는 승리한 색상을 출력하고, 두 번째 줄에는 승리한 다섯 개의 바둑 알 중 가운데 위치하는 바둑 알의 행과 열의 번호를 순서대로 출력하기 (단, 검은색 바둑 알은 1, 흰색 바둑 알은 2, 알이 놓이지 않은 자리는 0으로 주어짐)

---
+++ concave.py
@@ -0,0 +1,45 @@
+import sys
+# 19 * 19 크기의 오목판에 1(검은색 바둑알), 2(흰색 바둑알), 0(놓이지 않는 자리) 입력 받기
+arr = [
+    list(map(int, input().split()))
+    for _ in range(19)
+]
+
+# 좌표가 아닌 2차원 행렬로 생각해야 함
+# 따라서 dx가 세로열 / dy가 가로열
+# x := 아래로 이동시 +1 위로 이동시 -1
+# y := 오른쪽 이동시 +1 왼쪽 이동시 -1
+
+        # 북 북동 동 동남 남 남서 서 서북 
+dx, dy = [-1, -1, 0,  1,  1,  1, 0, -1], [0,  1,  1,  1,  0, -1, -1, -1]
+
+def in_range(x, y): # (x, y) 좌표가 19 * 19 크기의 오목판에 위치하는지 확인
+    return x >= 0 and x < 19 and y >= 0 and y < 19
+
+def is_same_color(i, j, x, y): # 한칸 앞으로 접근한 위치와 그 이전의 위치에 있는 바둑알 색상 같은지 확인
+    return arr[i][j] == arr[x][y]
+
+# x, y = 0, 0
+# 19 * 19 크기의 오목판을 행1 열1 위치부터 차례대로 탐색하는 반복문
+for i in range(19):
+    for j in range(19):
+        # 8가지의 방향을 모두 탐색하며, 같은 색상의 바둑알이 5개 있을 경우 승리
+        
+        if arr[i][j] == 0:
+            continue
+
+        for k in range(8): # 8가지의 방향의 dx, dy의 인덱스 := k
+            for l in range(1, 5): # (i, j)에서 앞으로 나아가는 횟수 := l
+                x, y = i + dx[k] * l, j + dy[k] * l # 8가지의 방향 중 4번씩 앞으로 나아갔을 때 4가지의 각각 위치
+                
+                if in_range(x, y) == False:
+                    break
+                if is_same_color(i, j, x, y) == False:
+                    break
+                    
+                if l == 4: # 5개의 바둑알 모두 오목판 벗어나지 않고, 같은 색상인 경우
+                    print(arr[i][j]) # 해당 위치의 색상 출력
+                    print(i + (2 * dx[k]) + 1, j + (2 * dy[k]) + 1) # 행과 열 숫자 구해야 하므로 index에서 1씩 더해줘야 함
+                    sys.exit()
+
+print(0)
