3b9bfa8c82bf91c61967a33571ab630ecac2f6d5
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-05-30 10:32:16 +0900

    Update areaOfRect.py

--- areaOfRect.py
+++ areaOfRect.py
@@ -1,14 +1,14 @@
 n = int(input()) # 점의 개수
+# n개의 각 점의 위치 (x, y) 공백 두고 입력 받기
 dots = [
     list(map(int, input().split()))
     for _ in range(n)
 ]
 
-INT_X, INT_Y = 0, 0
-for i in range(n): # 입력 받은 좌표 중 최댓값인 x와 y 구하기
+INT_X, INT_Y = 0, 0 # x축, y축 최댓값 나타내는 변수 0으로 초기화
+for i in range(n): # 입력 받은 좌표 중 최댓값인 x좌표와 y좌표 구하기 위한 반복문
     INT_X = max(INT_X, dots[i][0])
     INT_Y = max(INT_Y, dots[i][1])
-
 
 min_area = INT_X * INT_Y # 주어진 좌표들 기준 직사각형 넓이 최대일 때로 초기화
 
@@ -16,7 +16,7 @@ for i in range(n):
     min_x, max_x = INT_X , 0
     min_y, max_y = INT_Y, 0
 
-    # i번째 점 제외한 모든 점들의 최소,최대 x,y좌표 구하기
+    # i번째 점 제외한 모든 점들의 최소,최대 x,y좌표 구하기 위한 반복문
     for j in range(n):
         if j == i: # i번째 점 제외
             continue
