9ad83e321a94215523de24ade94e32023bea782c
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-06-08 00:08:00 +0900

    Create max_sum(2).py
    max_sum 문제를 재귀 함수를 이용해 푸는 방법. x, y 사이의 각 자리 숫자의 합 중 최댓값 출력

---
+++ max_sum(2).py
@@ -0,0 +1,24 @@
+# 변수 선언 및 입력
+x, y = tuple(map(int, input().split()))
+
+
+# 각 자리 숫자의 합을 구하여 반환하는 재귀 함수
+def digit_sum(n):
+    if n < 10:
+        return n
+    # 두 자리 이상의 숫자라면,
+    # 맨 끝자리를 제외한 숫자들의 합을 재귀적으로 호출한 뒤,
+    # 그 결과가 마지막 자릿수를 더해 반환
+    else:
+        return digit_sum(n // 10) + (n % 10)
+
+
+ans = 0
+
+# 각 숫자 i에 대해 
+# 각 자리 숫자의 합을 구해
+# 그 중 최댓값을 계산
+for i in range(x, y + 1):
+    ans = max(ans, digit_sum(i))
+    
+print(ans)
